a9a9cb1ed1d11c3c6b690ce28ce0fb2f4967c316
Mouheb Mahfoudhi <mouhebmahfoudhi8@gmail.com>
2026-05-02 17:51:02 +0100

    added vpc flow logs by default

--- terraform/main.tf
+++ terraform/main.tf
@@ -99,6 +99,17 @@ resource "aws_vpc" "main" {
   })
 }
 
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.main.id
+
+  ingress = []
+  egress  = []
+
+  tags = merge(local.tags, {
+    Name = "${local.name_prefix}-default-sg"
+  })
+}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -284,6 +295,63 @@ resource "aws_cloudwatch_log_group" "app" {
   name              = "/ecs/${var.project_name}"
   retention_in_days = var.log_retention_days
   kms_key_id        = aws_kms_key.app.arn
+
+  tags = local.tags
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow" {
+  name              = "/vpc/${var.project_name}/flow-logs"
+  retention_in_days = var.log_retention_days
+  kms_key_id        = aws_kms_key.app.arn
+
+  tags = local.tags
+}
+
+resource "aws_iam_role" "vpc_flow_logs" {
+  name = "${local.name_prefix}-vpc-flow-logs"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_role_policy" "vpc_flow_logs" {
+  role = aws_iam_role.vpc_flow_logs.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_flow_log" "vpc_flow" {
+  vpc_id               = aws_vpc.main.id
+  traffic_type         = "ALL"
+  log_destination_type = "cloud-watch-logs"
+  log_destination      = aws_cloudwatch_log_group.vpc_flow.arn
+  iam_role_arn         = aws_iam_role.vpc_flow_logs.arn
 
   tags = local.tags
 }
