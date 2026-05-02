24d658f35e7bd0558f2839fca7f64bad4b721ed0
Mouheb Mahfoudhi <mouhebmahfoudhi8@gmail.com>
2026-05-02 16:59:42 +0100

    moved ecs service to a private network and added a nat gateway

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -28,6 +28,12 @@ variable "public_subnet_cidrs" {
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Private subnet CIDR blocks"
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
 variable "availability_zones" {
   type        = list(string)
   description = "Availability zones to use"
